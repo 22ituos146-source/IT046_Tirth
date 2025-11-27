f7cb4efdff25cafb8c3d0c140dfbc138fb5a4ccc
diyaprajapati <diyabprajapati2005@gmail.com>
2025-11-27 10:18:40 +0530

    add and mul ans done

---
+++ IT046_Tirth-main/hello.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter x :");
+        int a=sc.nextInt();
+        System.out.println("enter y:");
+        int b=sc.nextInt();
+
+        int c=a+b;
+        int d=a-b;
+
+        System.out.println("add : "+ c +" ");
+        System.out.println("add : "+ d +" ");
+}
+}
